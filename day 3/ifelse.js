1b839e8ccc087270745297f338832dd823c6022e
1915077 <54636895+1915077@users.noreply.github.com>
2022-01-28 19:18:29 +0530

    day 3 assignment submitted

---
+++ day 3/ifelse.js
@@ -0,0 +1,9 @@
+const email=prompt("ENTER YOUR EMAIL");
+const password=prompt("ENTER YOUR PASSWORD")
+
+if(email=="stu@letsupgrade.com" && password==12345){
+alert("You are logged in");
+}
+else{
+alert("Try again with correct email and password");	
+}
