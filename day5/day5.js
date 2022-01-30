ef15dfce94635bed09287fec805492baa364ced9
1915077 <54636895+1915077@users.noreply.github.com>
2022-01-30 18:04:04 +0530

    assignment day5 submitted

---
+++ day5/day5.js
@@ -0,0 +1,18 @@
+function changeStyle(){
+        var element = document.getElementById("myDiv");
+        element.style.backgroundColor = "green";
+ }       
+ function change (){ 
+        var element = document.getElementById("myDiv1");
+        element.style.backgroundColor = "orange";
+    }
+    function changeon (){ 
+     var element = document.getElementById("myDiv2");
+        element.style.backgroundColor = "red";  
+    }
+    function changeme (){ 
+      var element = document.getElementById("myDiv3");
+        element.style.backgroundColor = "blue";     
+    }
+        
+        
